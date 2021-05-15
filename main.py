739ed81020e2d2c5fa9cea2a566c356c33c6d2e8
Dina Graves Portman <dinagraves@google.com>
2021-05-15 12:36:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "Foobar")
     return "Hello {}!".format(name)
 
 
